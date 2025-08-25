c72b99c52ba8137d66b08c8dbb32daa90668b571
Daniel Tonon <dan503.npm@gmail.com>
2025-08-25 18:17:50 +1000

    Set up working React hook variant tests

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,8 @@
-import { describe, it } from 'vitest'
+import { act, renderHook } from '@testing-library/react'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { create, type UseBoundStore } from 'zustand'
+import { createStore, type StoreApi } from 'zustand/vanilla'
 import { createSetterFn, type SetStateFn } from '.'
-import { createStore } from 'zustand/vanilla'
-import { expect } from 'vitest'
 
 interface CounterStore {
 	count: number
@@ -38,6 +39,42 @@ describe('createSetterFn', () => {
 			expect(store.getState().count).toBe(25)
 			setCount((prev) => prev - 1)
 			expect(store.getState().count).toBe(24)
+		})
+	})
+
+	describe('React hook variant', () => {
+		let useCounterStore: UseBoundStore<StoreApi<CounterStore>>
+
+		beforeEach(() => {
+			useCounterStore = create<CounterStore>()((set) => {
+				const setCount = createSetterFn(set, 'count')
+				return {
+					count: 0,
+					setCount,
+					increment: () => setCount((oldCount) => oldCount + 1),
+				}
+			})
+		})
+
+		it('can get the count value', () => {
+			const { result } = renderHook(() => useCounterStore())
+			expect(result.current.count).toBe(0)
+		})
+
+		it('can increment the value', () => {
+			const { result } = renderHook(() => useCounterStore())
+			act(() => result.current.increment())
+			expect(result.current.count).toBe(1)
+			act(() => result.current.increment())
+			expect(result.current.count).toBe(2)
+		})
+
+		it('can force set the value', () => {
+			const { result } = renderHook(() => useCounterStore())
+			act(() => result.current.setCount(25))
+			expect(result.current.count).toBe(25)
+			act(() => result.current.setCount((prev) => prev - 1))
+			expect(result.current.count).toBe(24)
 		})
 	})
 })
