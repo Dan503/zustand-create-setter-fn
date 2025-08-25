5a3f237ba70df37f2c54e53f9ed19e9815c13fb3
Daniel Tonon <dan503.npm@gmail.com>
2025-08-25 20:22:26 +1000

    Implemented the React hook method

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { createSetterFn, type SetStateFn } from './core'
+import { create } from 'zustand'
+
+interface CounterStore {
+	count: number
+	setCount: SetStateFn<number>
+	increment: () => void
+}
+
+const useCounterStore = create<CounterStore>()((set) => {
+	// Use this function like you would any React setState function
+	const setCount = createSetterFn(set, 'count')
+	return {
+		count: 0,
+		setCount,
+		increment: () => setCount((oldCount) => oldCount + 1),
+	}
+})
 
 function App() {
-  const [count, setCount] = useState(0)
+	const { count, increment, setCount } = useCounterStore()
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<h1>
+				Zustand <code>createSetterFn</code>
+			</h1>
+			<div
+				className="card"
+				style={{ display: 'flex', gap: '1em', alignItems: 'center' }}
+			>
+				<label style={{ display: 'flex', gap: '1em' }}>
+					<span>The count is</span>
+					<input
+						type="text"
+						value={count}
+						onChange={(e) => {
+							const value = parseInt(e.target.value)
+							if (isNaN(value)) {
+								return
+							}
+							setCount(value)
+						}}
+					/>
+				</label>
+				<button onClick={increment}>increment</button>
+			</div>
+		</>
+	)
 }
 
 export default App
