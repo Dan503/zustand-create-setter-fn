18529b2af3b7c925a74194a4a872576f984e34c4
Daniel Tonon <dan503.npm@gmail.com>
2025-08-27 15:04:10 +1000

    moved setup into the `testsFor` function

--- src/examples/tests.ts
+++ src/examples/tests.ts
@@ -3,32 +3,32 @@ import userEvent from '@testing-library/user-event'
 import type { ReactElement } from 'react'
 import { beforeEach, expect, test } from 'vitest'
 
-async function setup(component: ReactElement) {
-	render(component)
-	const user = userEvent.setup()
-	const inputElem = screen.getByRole<HTMLInputElement>('textbox')
-	const incrementBtn = screen.getByRole<HTMLButtonElement>('button', {
-		name: /increment/i,
-	})
-	const resetBtn = screen.getByRole<HTMLButtonElement>('button', {
-		name: /reset/i,
-	})
-
-	// Need to reset the state as part of the set up
-	await userEvent.click(resetBtn)
-
-	return { user, inputElem, incrementBtn, resetBtn }
-}
-
 export function testsFor(component: ReactElement) {
+	async function setup() {
+		render(component)
+		const user = userEvent.setup()
+		const inputElem = screen.getByRole<HTMLInputElement>('textbox')
+		const incrementBtn = screen.getByRole<HTMLButtonElement>('button', {
+			name: /increment/i,
+		})
+		const resetBtn = screen.getByRole<HTMLButtonElement>('button', {
+			name: /reset/i,
+		})
+
+		// Need to reset the state as part of the set up
+		await userEvent.click(resetBtn)
+
+		return { user, inputElem, incrementBtn, resetBtn }
+	}
+
 	beforeEach(() => cleanup())
 
 	test('starts at count 0', async () => {
-		const { inputElem } = await setup(component)
+		const { inputElem } = await setup()
 		expect(inputElem.value).toBe('0')
 	})
 	test('incrementing increases the value', async () => {
-		const { inputElem, incrementBtn } = await setup(component)
+		const { inputElem, incrementBtn } = await setup()
 
 		await userEvent.click(incrementBtn)
 		expect(inputElem.value).toBe('1')
@@ -38,7 +38,7 @@ export function testsFor(component: ReactElement) {
 	})
 
 	test('can reset the value', async () => {
-		const { inputElem, incrementBtn, resetBtn } = await setup(component)
+		const { inputElem, incrementBtn, resetBtn } = await setup()
 
 		await userEvent.click(incrementBtn)
 		expect(inputElem.value).toBe('1')
@@ -48,14 +48,14 @@ export function testsFor(component: ReactElement) {
 	})
 
 	test('can update the value', async () => {
-		const { inputElem } = await setup(component)
+		const { inputElem } = await setup()
 
 		await userEvent.type(inputElem, '32')
 		expect(inputElem.value).toBe('32')
 	})
 
 	test('will increment typed value', async () => {
-		const { inputElem, incrementBtn } = await setup(component)
+		const { inputElem, incrementBtn } = await setup()
 
 		await userEvent.type(inputElem, '32')
 		expect(inputElem.value).toBe('32')
@@ -65,7 +65,7 @@ export function testsFor(component: ReactElement) {
 	})
 
 	test('will reset the typed value', async () => {
-		const { inputElem, resetBtn } = await setup(component)
+		const { inputElem, resetBtn } = await setup()
 
 		await userEvent.type(inputElem, '32')
 		expect(inputElem.value).toBe('32')
